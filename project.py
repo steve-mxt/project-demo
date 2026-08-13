383f0dfea2d4f04dc8cf1192c750648d1340302d
steve-mxt <shinobininja031@gmail.com>
2026-08-13 20:51:11 +0530

    Add python new project

--- project.py
+++ project.py
@@ -1,5 +1,14 @@
 #userinput table 
-a=input("Enter a number: ")
+'''pythona=input("Enter a number: ")
 a=int(a)
 for i in range(1,11):
-    print(a,"x",i,"=",a*i)
+    print(a,"x",i,"=",a*i) '''
+    
+    #nested for loop
+rows=int(input("enter the  no.of row:"))
+cols=int(input("enter the no.of cols:"))
+symbol=input("enter the symbol:")
+for i in range(rows):
+        for j in range(cols):
+            print(symbol,end="")
+        print()    
